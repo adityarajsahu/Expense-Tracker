30db6294a0169a1ef6489fe57ccce11a909657fa
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2022-06-09 15:19:00 +0530

    new commiy

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 6, 1);
-  const expenseTitle = 'House Rent';
-  const expenseAmount = 20000;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">Rs. {expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs. {props.amount}</div>
       </div>
     </div>
   );
